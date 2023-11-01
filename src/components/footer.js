6beb297dea026c6c7a33fcd786d87c2a4eee1763
Hanzala1101 <110152360+Hanzala1101@users.noreply.github.com>
2023-11-01 08:46:18 +0000

    Feat: Optimize and Fav icon change

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function footer() {
   return (
     <div className="flex bg-slate-500 justify-between px-10" >
-      <div className="text-lg flex">Hanzala &nbsp; &nbsp; &nbsp; Image Gallery app</div>
+      <div className="text-lg flex">Image Gallery app</div>
       <div className="text-lg">&#169; 2023</div>
     </div>
   );
